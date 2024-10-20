ce58d472826ac3fac7a2e4a1a70f4d2d44a81446
MixelMeza <mixolms@gmail.com>
2024-10-20 10:21:43 -0500

    v1

--- server.js
+++ server.js
@@ -8,75 +8,75 @@ const connection = require('./db');
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/carreras', (req, res)=>{
-    const sql = 'SELECT *FROM carrera';
+app.get('/api/productos', (req, res)=>{
+    const sql = 'SELECT *FROM producto';
     connection.query(sql,(err, results)=>{
         if(err){
-            console.log('Error en la consulta de Carreras',err);
+            console.log('Error en la consulta de Productos',err);
             return res.status(500).send('Error del servidor');
         }else{
             res.status(200).send(results);
         }
     });
 });
-app.post('/api/carreras', (req, res) => {
-    const { nombre, estado } = req.body;
-    const query = 'INSERT INTO carrera (nombre, estado) VALUES (?, ?)';
+app.post('/api/productos', (req, res) => {
+    const { nombre, precio, cantidad } = req.body;
+    const query = 'INSERT INTO producto (nombre, precio, cantidad) VALUES (?, ?, ?)';
     
-    connection.query(query, [nombre, estado], (err, results) => {
+    connection.query(query, [nombre, precio, cantidad], (err, results) => {
       if (err) {
-        console.error('Error al insertar carrera:', err);
+        console.error('Error al insertar producto:', err);
         return res.status(500).send('Error en el servidor');
       }
-      res.status(201).send({ nombre, estado});
+      res.status(201).send({ nombre, precio, cantidad});
     });
   });
 
   // Actualizar (PUT)
-app.put('/api/carreras/:id', (req, res) => {
+app.put('/api/productos/:id', (req, res) => {
     const { id } = req.params;
-    const { nombre, estado } = req.body;
-    const query = 'UPDATE carrera SET nombre = ?, estado = ? WHERE idcarrera = ?';
+    const { nombre, precio, cantidad } = req.body;
+    const query = 'UPDATE producto SET nombre = ?, precio = ?, cantidad = ? WHERE idproducto = ?';
     
-    connection.query(query, [nombre, estado, id], (err, results) => {
+    connection.query(query, [nombre, precio, cantidad, id], (err, results) => {
       if (err) {
-        console.error('Error al actualizar carrera:', err);
+        console.error('Error al actualizar producto:', err);
         return res.status(500).send('Error en el servidor');
       }
       if (results.affectedRows === 0) {
-        return res.status(404).send('carrera no encontrado');
+        return res.status(404).send('producto no encontrado');
       }
-      res.status(200).send({ id, nombre, estado });
+      res.status(200).send({ id, nombre, precio, cantidad });
     });
   });
   
-  app.delete('/api/carreras/:id', (req, res) => {
+  app.delete('/api/productos/:id', (req, res) => {
     const { id } = req.params;
-    const query = 'DELETE FROM carrera WHERE idcarrera = ?';
+    const query = 'DELETE FROM producto WHERE idproducto = ?';
     
     connection.query(query, [id], (err, results) => {
       if (err) {
-        console.error('Error al eliminar Carrera:', err);
+        console.error('Error al eliminar Producto:', err);
         return res.status(500).send('Error en el servidor');
       }
       if (results.affectedRows === 0) {
-        return res.status(404).send('Carrera no encontrada');
+        return res.status(404).send('Producto no encontrada');
       }
-      res.status(200).send('Carrera eliminada');
+      res.status(200).send('Producto eliminada');
     });
   });
-  //Traer Carrera por ID
-  app.get('/api/carreras/:id', (req, res) => {
+  //Traer Producto por ID
+  app.get('/api/productos/:id', (req, res) => {
     const { id } = req.params;
-    const query = 'SELECT *FROM carrera WHERE idcarrera = ?';
+    const query = 'SELECT *FROM producto WHERE idproducto = ?';
     
     connection.query(query, [id], (err, results) => {
       if (err) {
-        console.error('Error al actualizar carrera:', err);
+        console.error('Error al actualizar producto:', err);
         return res.status(500).send('Error en el servidor');
       }
       if (results.affectedRows === 0) {
-        return res.status(404).send('carrera no encontrado');
+        return res.status(404).send('producto no encontrado');
       }
       res.status(200).send({ results});
     });
